d25a1d1833e09a2e5c61605a71666bb81ffb7aae
André Lins <andrelucas01@hotmail.com>
2020-02-16 15:44:29 -0300

    Option json return in js config

--- src/compilerVariables.js
+++ src/compilerVariables.js
@@ -5,7 +5,13 @@ const jsonToDotNotate = require("./utils/jsonToDotNotate");
 
 const compilerVariables = (path, extension) => {
   if (extension === "js") {
-    return jsonToDotNotate(require(path)());
+    const fileConfig = require(path);
+
+    if (typeof fileConfig === "function") {
+      return jsonToDotNotate(fileConfig());
+    }
+
+    return jsonToDotNotate(fileConfig);
   } else {
     return jsonToDotNotate(fs.readFileSync(path));
   }
